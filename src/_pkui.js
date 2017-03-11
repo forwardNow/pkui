90bc673ce1eeca8b7f284bda979b6bc8ec62e7f7
wuqinfei <wuqinfei_cs@163.com>
2017-03-11 12:18:51 +0800

    【PKUI】添加框架的核心功能

--- src/_pkui.js
+++ src/_pkui.js
@@ -1,18 +1,318 @@
 ;+function ( window ) {
-    var PKUI
-    ;
-    PKUI = {
-        responseDataHandler: function ( responseData ) {
-            if ( typeof responseData === "string" ) {
-                responseData = ( new Function( "return " + responseData ) )();
+    var
+
+        PKUI = {},
+
+
+        class2type = {},
+
+        hasOwn = class2type.hasOwnProperty,
+
+        toString = class2type.toString,
+
+        support = {},
+
+        rvalidtokens = /(,)|(\[|{)|(}|])|"(?:[^"\\\r\n]|\\["\\\/bfnrt]|\\u[\da-fA-F]{4})*"\s*:?|true|false|null|-?(?!0\d)\d+(?:\.\d+|)(?:[eE][+-]?\d+|)/g,
+
+        // Support: Android<4.1, IE<9
+        // Make sure we trim BOM and NBSP
+        rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g
+
+        ;
+
+
+    PKUI.extend = function () {
+        var src, copyIsArray, copy, name, options, clone,
+            target = arguments[ 0 ] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+
+            // skip the boolean and the target
+            target = arguments[ i ] || {};
+            i++;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !PKUI.isFunction( target ) ) {
+            target = {};
+        }
+
+        // extend PKUI itself if only one argument is passed
+        if ( i === length ) {
+            target = this;
+            i--;
+        }
+
+        for ( ; i < length; i++ ) {
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null ) {
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( PKUI.isPlainObject( copy ) || (copyIsArray = PKUI.isArray( copy )) ) ) {
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && PKUI.isArray( src ) ? src : [];
+
+                        } else {
+                            clone = src && PKUI.isPlainObject( src ) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = PKUI.extend( deep, clone, copy );
+
+                        // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
+                    }
+                }
             }
-            if ( ! responseData.success ) {
-                console.info( "/(ㄒoㄒ)/~~获取数据失败！" );
+        }
+
+        // Return the modified object
+        return target;
+    };
+
+    PKUI.extend( {
+        type: function ( obj ) {
+            if ( obj == null ) {
+                return obj + "";
+            }
+            return typeof obj === "object" || typeof obj === "function" ?
+                class2type[ toString.call( obj ) ] || "object" :
+                typeof obj;
+        },
+        // See test/unit/core.js for details concerning isFunction.
+        // Since version 1.3, DOM methods and functions like alert
+        // aren't supported. They return false on IE (#2968).
+        isFunction: function( obj ) {
+            return PKUI.type(obj) === "function";
+        },
+        isWindow: function( obj ) {
+            /* jshint eqeqeq: false */
+            return obj != null && obj == obj.window;
+        },
+        isArray: Array.isArray || function( obj ) {
+            return PKUI.type(obj) === "array";
+        },
+        isPlainObject: function( obj ) {
+            var key;
+
+            // Must be an Object.
+            // Because of IE, we also have to check the presence of the constructor property.
+            // Make sure that DOM nodes and window objects don't pass through, as well
+            if ( !obj || PKUI.type(obj) !== "object" || obj.nodeType || PKUI.isWindow( obj ) ) {
                 return false;
             }
-            return responseData.data;
+
+            try {
+                // Not own constructor property must be Object
+                if ( obj.constructor &&
+                    !hasOwn.call(obj, "constructor") &&
+                    !hasOwn.call(obj.constructor.prototype, "isPrototypeOf") ) {
+                    return false;
+                }
+            } catch ( e ) {
+                // IE8,9 Will throw exceptions on certain host objects #9897
+                return false;
+            }
+
+            // Support: IE<9
+            // Handle iteration over inherited properties before own properties.
+            if ( support.ownLast ) {
+                for ( key in obj ) {
+                    return hasOwn.call( obj, key );
+                }
+            }
+
+            // Own properties are enumerated firstly, so to speed up,
+            // if last one is own, then all properties are own.
+            for ( key in obj ) {}
+
+            return key === undefined || hasOwn.call( obj, key );
         }
-    };
+    } );
+
+    PKUI.extend( {
+        // args is for internal usage only
+        each: function ( obj, callback, args ) {
+            var value,
+                i = 0,
+                length = obj.length,
+                isArray = isArraylike( obj );
+
+            if ( args ) {
+                if ( isArray ) {
+                    for ( ; i < length; i++ ) {
+                        value = callback.apply( obj[ i ], args );
+
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                } else {
+                    for ( i in obj ) {
+                        value = callback.apply( obj[ i ], args );
+
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                }
+
+                // A special, fast, case for the most common use of each
+            } else {
+                if ( isArray ) {
+                    for ( ; i < length; i++ ) {
+                        value = callback.call( obj[ i ], i, obj[ i ] );
+
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                } else {
+                    for ( i in obj ) {
+                        value = callback.call( obj[ i ], i, obj[ i ] );
+
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return obj;
+        },
+        trim: function ( text ) {
+            return text == null ?
+                "" :
+                ( text + "" ).replace( rtrim, "" );
+        },
+        error: function ( msg ) {
+            throw new Error( msg );
+        }
+    } );
+
+
+    PKUI.extend( {
+
+        parseJSON: function( data ) {
+        // Attempt to parse using the native JSON parser first
+        if ( window.JSON && window.JSON.parse ) {
+            // Support: Android 2.3
+            // Workaround failure to string-cast null input
+            return window.JSON.parse( data + "" );
+        }
+
+        var requireNonComma,
+            depth = null,
+            str = PKUI.trim( data + "" );
+
+        // Guard against invalid (and possibly dangerous) input by ensuring that nothing remains
+        // after removing valid tokens
+        return str && !PKUI.trim( str.replace( rvalidtokens, function( token, comma, open, close ) {
+
+            // Force termination if we see a misplaced comma
+            if ( requireNonComma && comma ) {
+                depth = 0;
+            }
+
+            // Perform no more replacements after returning to outermost depth
+            if ( depth === 0 ) {
+                return token;
+            }
+
+            // Commas must not follow "[", "{", or ","
+            requireNonComma = open || comma;
+
+            // Determine new depth
+            // array/object open ("[" or "{"): depth += true - false (increment)
+            // array/object close ("]" or "}"): depth += false - true (decrement)
+            // other cases ("," or primitive): depth += true - true (numeric cast)
+            depth += !close - !open;
+
+            // Remove this token
+            return "";
+        }) ) ?
+            ( Function( "return " + str ) )() :
+            PKUI.error( "Invalid JSON: " + data );
+    }
+    } );
+
+    // Populate the class2type map
+    PKUI.each( "Boolean Number String Function Array Date RegExp Object Error".split( " " ), function ( i, name ) {
+        class2type[ "[object " + name + "]" ] = name.toLowerCase();
+    } );
+
+    /**
+     * 通用功能
+     */
+    PKUI.extend( {
+        /**
+         * @example
+         * public class JsonResult {
+         *     private boolean success = false;
+         *     private String message = "";
+         *     private Object data;
+         * }
+         */
+        handleJsonResult: function ( jsonResult ) {
+            if ( typeof jsonResult === "string" ) {
+                jsonResult = PKUI.parseJSON( jsonResult );
+            }
+            if ( jsonResult.success == false ) {
+                return false;
+            }
+            return jsonResult;
+        },
+        /**
+         * @example
+         * public class GridResult {
+         *     private boolean success = true;
+         *     private int totalRecords = 0;
+         *     private List<?> data;
+         * }
+         */
+        handleGridResult: function ( gridResult ) {
+            return PKUI.handleJsonResult( gridResult );
+        }
+    } ) ;
+
+
+    function isArraylike( obj ) {
+
+        // Support: iOS 8.2 (not reproducible in simulator)
+        // `in` check used to prevent JIT error (gh-2145)
+        // hasOwn isn't used here due to false negatives
+        // regarding Nodelist length in IE
+        var length = "length" in obj && obj.length,
+            type = PKUI.type( obj );
+
+        if ( type === "function" || PKUI.isWindow( obj ) ) {
+            return false;
+        }
+
+        if ( obj.nodeType === 1 && length ) {
+            return true;
+        }
+
+        return type === "array" || length === 0 ||
+            typeof length === "number" && length > 0 && ( length - 1 ) in obj;
+    }
+
 
     // namespace
     window.PKUI = PKUI;
